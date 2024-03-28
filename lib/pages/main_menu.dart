0af2407e288f528882504ccb1f244e58ac9ee3b5
Nicholas Amancio <nick.amancio44@gmail.com>
2024-03-27 23:43:26 -0700

    fixed the problem with the setState function not loading the newly added expenses to the main_menu. Used the same logic to fix the Sign out button if say the user presses the System's Back Button

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -33,159 +33,171 @@ class _MainMenuState extends State<MainMenu> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Container(
-              height: 407.143,
-              color: Color(0xFF3C9FE7),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    child: TextButton(
-                        onPressed: () {
-                          FirebaseAuth.instance.signOut();
-                          Navigator.pop(context);
-                        },
-                        child: Text("Sign out")),
-                  ),
-                  Center(
-                    child: Image(
-                      image: AssetImage('assets/tree5.png'),
+    return PopScope(
+      canPop: true,
+      onPopInvoked: (didPop) async {
+        if (didPop) {
+          await Future.delayed(Duration.zero, () {
+            FirebaseAuth.instance.signOut();
+            Navigator.pushReplacementNamed(context, '/');
+          });
+          return;
+        }
+      },
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                height: 407.143,
+                color: Color(0xFF3C9FE7),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                      child: TextButton(
+                          onPressed: () {
+                            FirebaseAuth.instance.signOut();
+                            Navigator.pop(context);
+                          },
+                          child: Text("Sign out")),
                     ),
-                  ),
-                ],
+                    Center(
+                      child: Image(
+                        image: AssetImage('assets/tree5.png'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SliverFillRemaining(
-            fillOverscroll: true,
-            hasScrollBody: false,
-            child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.0, 0.5, 0.5 , 1.0],
-                    colors: [Color(0xFF58E47F), Color(0xFF58E47F) , Color(0xFFD2D2D2), Color(0xFFD2D2D2)],
-                  )
-              ),
-              //At a glance Column
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 30, 20, 40),
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(10, 10, 20, 0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 3),
-                          )
-                        ],
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("At a Glance", style: TextStyle(color: Color(0xF22F2F2F), fontSize: 24, fontWeight: FontWeight.w500,)),
-                          Text("\$${budgetModel.calculateAmountSpent(purchases).toStringAsFixed(2)}", style: TextStyle(color: Color(0xF22F2F2F), fontSize: 16, letterSpacing: 1, fontWeight: FontWeight.w500,)),
-                          Text("Your spending as of ${calculateDate()}", style: TextStyle(color: Color(0xF22F2F2F), fontSize: 12, fontWeight: FontWeight.w400)),
-                          SizedBox(height: 50),
-                          Chart(budgetModel: budgetModel, purchases: purchases),
-                          SizedBox(height: 100),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 50),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            SliverFillRemaining(
+              fillOverscroll: true,
+              hasScrollBody: false,
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.5, 0.5 , 1.0],
+                      colors: [Color(0xFF58E47F), Color(0xFF58E47F) , Color(0xFFD2D2D2), Color(0xFFD2D2D2)],
+                    )
+                ),
+                //At a glance Column
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 30, 20, 40),
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(10, 10, 20, 0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            )
+                          ],
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Column(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pushNamed(context, '/plan', arguments: {
-                                      'userModel': userModel,
-                                      'budgetModel': budgetModel,
-                                      'purchaseList': purchases,
-                                    });
-                                  },
-                                  style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
-                                  child: Image.network('https://img.icons8.com/sf-regular/48/literature.png', width: 32, height: 32,),
-                                ),
-                                SizedBox(height: 8),
-                                Text("Plan", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pushNamed(context, '/track', arguments: {
-                                      'userModel': userModel,
-                                      'budgetModel': budgetModel,
-                                      'purchaseList': purchases,
-                                    });
-                                  },
-                                  style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
-                                  child: Image.network('https://img.icons8.com/material-outlined/24/graph.png', width: 32, height: 32,),
-                                ),
-                                SizedBox(height: 8),
-                                Text("Track", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pushNamed(context, '/goal_page', arguments: {
-                                      'userModel': userModel,
-                                    });
-                                  },
-                                  style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
-                                  child: Image.network('https://img.icons8.com/fluency-systems-regular/48/trophy--v1.png', width: 32, height: 32,),
-                                ),
-                                SizedBox(height: 8),
-                                Text("Goals", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pushNamed(context, '/settings', arguments: {
-                                      'userModel': userModel,
-                                    });
-                                  },
-                                  style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
-                                  child: Image.network('https://img.icons8.com/?size=256&id=82535&format=png', width: 32, height: 32,),
-                                ),
-                                SizedBox(height: 8),
-                                Text("Settings", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
-                              ],
-                            ),
+                            Text("At a Glance", style: TextStyle(color: Color(0xF22F2F2F), fontSize: 24, fontWeight: FontWeight.w500,)),
+                            Text("\$${budgetModel.calculateAmountSpent(purchases).toStringAsFixed(2)}", style: TextStyle(color: Color(0xF22F2F2F), fontSize: 16, letterSpacing: 1, fontWeight: FontWeight.w500,)),
+                            Text("Your spending as of ${calculateDate()}", style: TextStyle(color: Color(0xF22F2F2F), fontSize: 12, fontWeight: FontWeight.w400)),
+                            SizedBox(height: 50),
+                            Chart(budgetModel: budgetModel, purchases: purchases),
+                            SizedBox(height: 100),
                           ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 50),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, '/plan', arguments: {
+                                        'userModel': userModel,
+                                        'budgetModel': budgetModel,
+                                        'purchaseList': purchases,
+                                      });
+                                    },
+                                    style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
+                                    child: Image.network('https://img.icons8.com/sf-regular/48/literature.png', width: 32, height: 32,),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text("Plan", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, '/track', arguments: {
+                                        'userModel': userModel,
+                                        'budgetModel': budgetModel,
+                                        'purchaseList': purchases,
+                                      });
+                                    },
+                                    style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
+                                    child: Image.network('https://img.icons8.com/material-outlined/24/graph.png', width: 32, height: 32,),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text("Track", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, '/goal_page', arguments: {
+                                        'userModel': userModel,
+                                      });
+                                    },
+                                    style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
+                                    child: Image.network('https://img.icons8.com/fluency-systems-regular/48/trophy--v1.png', width: 32, height: 32,),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text("Goals", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, '/settings', arguments: {
+                                        'userModel': userModel,
+                                      });
+                                    },
+                                    style: ElevatedButton.styleFrom(shape: CircleBorder(), padding: EdgeInsets.all(12),),
+                                    child: Image.network('https://img.icons8.com/?size=256&id=82535&format=png', width: 32, height: 32,),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text("Settings", style: TextStyle(color: Color(0xF22F2F2F), fontWeight: FontWeight.w500)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
